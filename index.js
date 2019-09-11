2c83ba4c42220f5810288b571a151472a52f3e7d
xWafl <35458851+xWafl@users.noreply.github.com>
2019-09-11 11:56:49 -0400

    gang

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => res.sendFile('index.js', {root:"."}))
+app.get('/:url', function (req, res) {
+  let leurl = req.params.url;
+  res.redirect(leurl);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
